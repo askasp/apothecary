456ef000a6069aa8814af1e7e192761177842bf0
Aksel <aksel@stadler.no>
2026-03-03 11:17:39 +0100

    Bump version to 0.1.3

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Apothecary.MixProject do
   def project do
     [
       app: :apothecary,
-      version: "0.1.0",
+      version: "0.1.3",
       elixir: "~> 1.15",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
